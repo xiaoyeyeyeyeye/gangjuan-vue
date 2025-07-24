4b66ecebb83fdd49a0d551bb70db70ec8c898c5c
liuqiaozhi <2411438681@qq.com>
2025-07-24 17:23:12 +0800

    Initial commit

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dashboard-container">
+    <!-- 原句 不同角色登录显示不同页面 -->
+    <!-- <component :is="currentRole" /> -->
+    <adminDashboard />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import adminDashboard from './admin'
+// import editorDashboard from './editor'
+
+export default {
+  name: 'Dashboard',
+  // components: { adminDashboard, editorDashboard },
+  components: { adminDashboard },
+  data() {
+    return {
+      currentRole: 'adminDashboard'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'roles'
+    ])
+  },
+  created() {
+    if (!this.roles.includes('admin')) {
+      this.currentRole = 'editorDashboard'
+    }
+  }
+}
+</script>
