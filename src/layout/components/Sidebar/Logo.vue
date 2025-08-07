e30d85e23ab1ba4b256b6625507309c85c969656
liuqiaozhi <2411438681@qq.com>
2025-08-07 10:53:09 +0800

    logo的显示与修改

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -7,7 +7,7 @@
       </router-link>
       <router-link v-else key="expand" class="sidebar-logo-link" to="/">
         <img v-if="logo" :src="logo" class="sidebar-logo">
-        <h1 class="sidebar-title">{{ title }} </h1>
+        <!-- <h1 class="sidebar-title">{{ title }} </h1> -->
       </router-link>
     </transition>
   </div>
@@ -25,7 +25,9 @@ export default {
   data() {
     return {
       title: 'Vue Element Admin',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      // logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      logo: require('@/assets/logos/LOGO_TEST.png')
+
     }
   }
 }
@@ -46,7 +48,8 @@ export default {
   width: 100%;
   height: 50px;
   line-height: 50px;
-  background: #2b2f3a;
+  // background: #2b2f3a;
+  background: #a6baf5;
   text-align: center;
   overflow: hidden;
 
@@ -55,7 +58,8 @@ export default {
     width: 100%;
 
     & .sidebar-logo {
-      width: 32px;
+      //width: 32px;
+      width: 130px;
       height: 32px;
       vertical-align: middle;
       margin-right: 12px;
